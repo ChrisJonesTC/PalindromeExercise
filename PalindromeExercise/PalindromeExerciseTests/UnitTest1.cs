661fe320d0043e73d7d8b986a0aae3f0099261d6
ChrisJonesTC <cfjones91@gmail.com>
2022-10-05 09:52:56 -0400

    exercise2

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
-    public class UnitTest1
+  public class UnitTest1
+  {
+    
+        [Theory]
+    [InlineData("racecar", true)]
+    [InlineData("deified", true)]
+    [InlineData("noon", true)]
+    [InlineData("rotator", true)]
+    [InlineData("firetruck", false)]
+    [InlineData("coding", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public void IsAPalindrome_Test(string word, bool expected)
     {
-        [Fact]
-        public void Test1()
-        {
-
-        }
+        //Arrange
+        var ws = new WordSmith();
+        //Act
+        var actual = ws.IsAPalindrome(word);
+        //Assert
+        Assert.Equal(expected, actual);
     }
+    
+  }
 }
